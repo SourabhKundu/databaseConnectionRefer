ffaddb403a4ffc172c3d860403fce96adfe2ee20
Sourabh Kundu <sourabhkundu45@gmail.com>
2021-08-29 15:28:11 +0530

    Reading_File

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,37 @@
 package com.company;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String deliveries = "./deliveries.csv";
-        File file1 = new File(pathMatches);
-        FileReader fr1 = new FileReader(file1);
+        try {
+            String deliveries = "src/com/company/deliveries.csv";
+            String matches = "src/com/company/matches.csv";
+            File file1 = new File(matches);
+            FileReader fr1 = new FileReader(file1);
+            File file2 = new File(deliveries);
+            FileReader fr2 = new FileReader(file2);
 
-        BufferedReader br = new BufferedReader(new FileReader("CSVDemo.csv"));
-        System.out.println("Hello World");
+
+            BufferedReader br = new BufferedReader(fr1);
+            BufferedReader br1 = new BufferedReader(fr2);
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 
 
