c78e251b75501865e8467e4000555e2fe0413ff1
Sourabh Kundu <sourabhkundu45@gmail.com>
2021-08-29 17:33:31 +0530

    Total Extra runs for teams

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -36,6 +36,7 @@ public class Main {
             //Hashmap
             HashMap<String,Integer> Matches_Played_perYear = new HashMap<String,Integer>();
             HashMap<String,Integer> Matches_won_Overall = new HashMap<String,Integer>();
+            HashMap<String,Integer> Extra_runs = new HashMap<String,Integer>();
 
             while((line=br.readLine())!=null){
                 String[] match = line.split(splitBy);
@@ -45,12 +46,33 @@ public class Main {
                 int count1 = Matches_won_Overall.containsKey(match[10]) ? Matches_won_Overall.get(match[10]) : 0;
                 Matches_won_Overall.put(match[10], count1 + 1);
                             }
+            while((line= br1.readLine())!=null){
+                String[] delivery = line.split(splitBy);
+                //System.out.println(delivery[2]+"  :  "+delivery[16]);
+                if(!delivery[16].startsWith("extra_runs")) {
+                    int Runs = Integer.parseInt(delivery[16]);
+
+                    int count3 = Extra_runs.containsKey(delivery[2]) ? Extra_runs.get(delivery[2]) : 0;
+
+                    Extra_runs.put(delivery[2], (count3 + Runs));
+                }
+                else{
+                    continue;
+                }
+
+
+
+            }
             Set<String> keys = Matches_Played_perYear.keySet();
             Set<String> keys2 = Matches_won_Overall.keySet();
+            Set<String> keys3 = Extra_runs.keySet();
+            for(String k : keys3){
+                System.out.println("Team "+k+"  :  "+ Extra_runs.get(k));
+            }
 
             for(String i : keys) {
                 if (i.startsWith("2")) {
-                    System.out.println("Year "+i + " Total Matches Played:" + Matches_Played_perYear.get(i));
+                    //System.out.println("Year "+i + " Total Matches Played:" + Matches_Played_perYear.get(i));
                 }
                 else{
                     continue;
@@ -58,7 +80,7 @@ public class Main {
             }
             for(String j : keys2) {
                 if (!j.startsWith("winner")) {
-                    System.out.println("Team " + j + " Won " + Matches_won_Overall.get(j) + " matches.");
+                    //System.out.println("Team " + j + " Won " + Matches_won_Overall.get(j) + " matches.");
                 } else {
                     continue;
                 }
