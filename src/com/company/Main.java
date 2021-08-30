e9c77066faf9226e1492bbc3dd793f99e7b17a4a
Sourabh Kundu <sourabhkundu45@gmail.com>
2021-08-30 09:02:28 +0530

    Extra Runs Optimise

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -37,6 +37,7 @@ public class Main {
             HashMap<String,Integer> Matches_Played_perYear = new HashMap<String,Integer>();
             HashMap<String,Integer> Matches_won_Overall = new HashMap<String,Integer>();
             HashMap<String,Integer> Extra_runs = new HashMap<String,Integer>();
+            HashMap<String,Integer> Economical_Bowler = new HashMap<String,Integer>();
 
             while((line=br.readLine())!=null){
                 String[] match = line.split(splitBy);
@@ -49,15 +50,23 @@ public class Main {
             while((line= br1.readLine())!=null){
                 String[] delivery = line.split(splitBy);
                 //System.out.println(delivery[2]+"  :  "+delivery[16]);
-                if(!delivery[16].startsWith("extra_runs")) {
+
+                if(!delivery[16].startsWith("extra_runs") ) {
+                    int matches_played = Integer.parseInt(delivery[0]);
+                    if(577<=matches_played && matches_played<=636){
                     int Runs = Integer.parseInt(delivery[16]);
 
                     int count3 = Extra_runs.containsKey(delivery[2]) ? Extra_runs.get(delivery[2]) : 0;
 
                     Extra_runs.put(delivery[2], (count3 + Runs));
-                }
+                }}
                 else{
                     continue;
+                }
+                if (!delivery[17].startsWith("total")){
+                    int Runs2 = Integer.parseInt(delivery[17]);
+                    int count4 = Economical_Bowler.containsKey(delivery[8]) ? Economical_Bowler.get(delivery[8]) : 0;
+                    Economical_Bowler.put(delivery[8],(count4+Runs2));
                 }
 
 
@@ -66,8 +75,12 @@ public class Main {
             Set<String> keys = Matches_Played_perYear.keySet();
             Set<String> keys2 = Matches_won_Overall.keySet();
             Set<String> keys3 = Extra_runs.keySet();
+            Set<String> keys4 = Economical_Bowler.keySet();
+            for(String l : keys4){
+                System.out.println("Bowler "+l + " Runs "+Economical_Bowler.get(l));
+            }
             for(String k : keys3){
-                System.out.println("Team "+k+"  :  "+ Extra_runs.get(k));
+                //System.out.println("Team "+k+"  :  "+ Extra_runs.get(k));
             }
 
             for(String i : keys) {
