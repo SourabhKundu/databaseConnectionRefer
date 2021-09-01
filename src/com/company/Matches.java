80ba3630665f885087bd3195d30cf84c5cb76498
Sourabh Kundu <sourabhkundu45@gmail.com>
2021-09-01 09:14:19 +0530

    Initialised getter setter for match.csv file

--- src/com/company/Matches.java
+++ src/com/company/Matches.java
@@ -1,15 +1,157 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-
 public class Matches {
-    public static void main(String[] args) throws FileNotFoundException {
-        String matches = "src/com/company/matches.csv";
-        File matchFile = new File(matches);
-        FileReader matchFileReader = new FileReader(matchFile);
-        BufferedReader matchReader = new BufferedReader(matchFileReader);
+    private int match_Id;
+    private String season;
+    private String city;
+    private String date;
+    private String team1;
+    private String team2;
+    private String toss_winner;
+    private String toss_decision;
+    private String result;
+    private int dl_applied;
+    private String winner;
+    private int win_By_Runs;
+    private int win_By_wickets;
+    private String playerOfTheMatch;
+    private String venue;
+    private String umpire1;
+    private String umpire2;
+
+    public int getMatch_Id() {
+        return match_Id;
+    }
+
+    public void setMatch_Id(int match_Id) {
+        this.match_Id = match_Id;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTeam1() {
+        return team1;
+    }
+
+    public void setTeam1(String team1) {
+        this.team1 = team1;
+    }
+
+    public String getTeam2() {
+        return team2;
+    }
+
+    public void setTeam2(String team2) {
+        this.team2 = team2;
+    }
+
+    public String getToss_winner() {
+        return toss_winner;
+    }
+
+    public void setToss_winner(String toss_winner) {
+        this.toss_winner = toss_winner;
+    }
+
+    public String getToss_decision() {
+        return toss_decision;
+    }
+
+    public void setToss_decision(String toss_decision) {
+        this.toss_decision = toss_decision;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public int getDl_applied() {
+        return dl_applied;
+    }
+
+    public void setDl_applied(int dl_applied) {
+        this.dl_applied = dl_applied;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+    public int getWin_By_Runs() {
+        return win_By_Runs;
+    }
+
+    public void setWin_By_Runs(int win_By_Runs) {
+        this.win_By_Runs = win_By_Runs;
+    }
+
+    public int getWin_By_wickets() {
+        return win_By_wickets;
+    }
+
+    public void setWin_By_wickets(int win_By_wickets) {
+        this.win_By_wickets = win_By_wickets;
+    }
+
+    public String getPlayerOfTheMatch() {
+        return playerOfTheMatch;
+    }
+
+    public void setPlayerOfTheMatch(String playerOfTheMatch) {
+        this.playerOfTheMatch = playerOfTheMatch;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public String getUmpire1() {
+        return umpire1;
+    }
+
+    public void setUmpire1(String umpire1) {
+        this.umpire1 = umpire1;
+    }
+
+    public String getUmpire2() {
+        return umpire2;
+    }
+
+    public void setUmpire2(String umpire2) {
+        this.umpire2 = umpire2;
     }
 }
